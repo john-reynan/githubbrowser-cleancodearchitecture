af28503beefa57b2bb2b2f51d583acc93b6d09e6
unknown <johnreynan.deocampo@gmail.com>
2021-03-12 17:41:15 +0800

    updated GitRepo model

--- domain/src/main/java/com/reynandeocampo/domain/models/GitRepo.kt
+++ domain/src/main/java/com/reynandeocampo/domain/models/GitRepo.kt
@@ -2,7 +2,10 @@ package com.reynandeocampo.domain.models
 
 data class GitRepo(
     var owner: Owner,
-    var url: String,
+    var title: String,
     var description: String? = null,
+    var url: String,
+    var createdAt: String,
+    var updatedAt: String,
     var id: Long = 0
 )
